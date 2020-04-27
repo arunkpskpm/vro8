3d3e295a9a7280d8df7ba6397adf893a6ae918a1
scott <scott@mail.com>
2020-04-27 13:59:08 +0000

    major release

---
+++ ActionBundles/com.terasky.utils/json2yaml_pwsh/index.ps1
@@ -0,0 +1,12 @@
+﻿import-module powershell-yaml
+$ErrorActionPreference = "Stop"
+function Handler {
+  Param($context, $inputs)
+  $PsJson = $inputs.jsonStr
+  # Convert JSON back to PowerShell Array
+  $PsArray = @($PsJson | ConvertFrom-Json)
+
+  # Convert the Array to YAML
+  $PsYaml = ConvertTo-Yaml -Data $PsArray
+return $PsYaml
+}
---
+++ ActionBundles/com.terasky.utils/yaml2json_pwsh/yaml2json.ps1
@@ -0,0 +1,10 @@
+﻿import-module powershell-yaml
+$ErrorActionPreference = "Stop"
+function Handler {
+  Param($context, $inputs)
+  $PsYaml = $inputs.yamlTxt
+  # Convert JSON back to PowerShell Array
+  $PsJson = (ConvertFrom-Yaml -Yaml $PsYaml -Ordered | ConvertTo-Json)
+
+return $PsJson
+}
